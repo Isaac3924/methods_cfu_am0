24151903561f126dc3553e44f8070ad1e5b14f03
Isaac Alter <i_alter@yahoo.com>
2022-11-03 11:35:12 -0600

    Initial commit

--- define_and_id.rb
+++ define_and_id.rb
@@ -9,19 +9,29 @@
 
 # YOU DO the rest:
 
-
+# The include? method is called on the string object "Hello World"
+# The string "Hello" is passed as an argument; include searches the String it was called on for the argument provided.
+# Finding the argument, it returns the value 'true'.
 "Hello World".include?("Hello")
 
-
+# The end_with? method is called on the string object "Hello World"
+# The string "Hello" is passed as an argument; end_with checks the end of the String it was called on with the argument provided.
+# Unable to find the argument as the end of the String, it returns the value 'false'.
 "Hello World".end_with?("Hello")
 
-
+# The end_with? method is called on the string object "Hello World"
+# The string "rld" is passed as an argument; end_with checks the end of the String it was called on with the argument provided.
+# Finding the argument as the end of the String, it returns the value 'true'.
 "Hello World".end_with?("rld")
 
-
+# The even? method is called on the Int object 12
+# No arguments are passed; even? has one task and requires no parameters, merely the object, which it determines whether it is even or not.
+# Finding the Int object to be even, it returns the value 'true'.
 12.even?
 
-
+# The next method is called on the Int object 18
+# No arguments are passed; next has one task and requires no parameters, merely the object, which it increases by an Int value of 1.
+# It returns the new Int value, which is 19.
 18.next
 
 
